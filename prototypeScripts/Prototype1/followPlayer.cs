8436af3361ed696e4af11f6700959328830865e6
gabesharpton <gabriel.sharpton@gmail.com>
2020-07-17 14:23:15 -0500

    updated final scripts for Prototype1

--- prototypeScripts/Prototype1/followPlayer.cs
+++ prototypeScripts/Prototype1/followPlayer.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+
+    //Offset variable for camera
     private Vector3 offset = new Vector3(0, 5, -10);
+
     // Start is called before the first frame update
     void Start()
     {
--- prototypeScripts/Prototype1/playerController.cs
+++ prototypeScripts/Prototype1/playerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
+    //Private variables
+    private float speed = 20.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +19,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // We'll move to vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //Where I am getting the player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // We move to vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //We turn the directon of vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
     }
 }
